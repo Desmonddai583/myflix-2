ffed3aa9b5397ff6d8f468be4064cbe520f9faf0
Desmond dai <desmonddai583@gmail.com>
2015-09-09 16:26:13 +0800

    week1 module3

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
-
+  root to: 'pages#front'
   resources :videos, only: [:show] do
     collection do
       get 'search', to: 'videos#search'
     end
   end
   get 'home', to: 'videos#index'
-
   resources :categories, only: [:show]
-
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
+  get 'ui(/:action)', controller: 'ui'
 end
